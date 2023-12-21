5befbdfea355664376e692a2b27ae82e28d673c4
ody <odykai2009@gmail.com>
2023-12-21 13:42:15 -0500

    no clue for 17

---
+++ leetcode/lc17_threeSum.py
@@ -0,0 +1,22 @@
+"""
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+"""
+
+class Solution:
+    def letterCombinations(self, digits: str) -> list[str]:
+
+        return result
+
+    
+
+if __name__ == "__main__":
+    solution = Solution()
+    test_1 = "23"
+    test_2 = ""
+    test_3 = "2"
+    ans = solution.letterCombinations(nums=test_3)
+    print(ans)
+ 
