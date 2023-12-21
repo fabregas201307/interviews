8394b219c007ded8536aebf749ba2e52540a6439
ody <odykai2009@gmail.com>
2023-12-21 13:57:28 -0500

    watched vediro

--- leetcode/lc17_threeSum.py
+++ leetcode/lc17_threeSum.py
@@ -7,6 +7,20 @@ A mapping of digits to letters (just like on the telephone buttons) is given bel
 
 class Solution:
     def letterCombinations(self, digits: str) -> list[str]:
+        my_dict = dict()
+        my_dict["2"] = ['a', 'b', 'c']
+        my_dict["3"] = ['d', 'e', 'f']
+        my_dict["4"] = ['g', 'h', 'i']
+        my_dict["5"] = ['j', 'k', 'l']
+        my_dict["6"] = ['m', 'n', 'o']
+        my_dict["7"] = ['p', 'q', 'r', 's']
+        my_dict["8"] = ['t', 'u', 'v']
+        my_dict["9"] = ['w', 'x', 'y', 'z']
+
+        result = my_dict.get(digits[0])
+
+        for digit in digits[1:]:
+            result = [old+new for old in result for new in my_dict.get(digit)]
 
         return result
 
@@ -17,6 +31,6 @@ if __name__ == "__main__":
     test_1 = "23"
     test_2 = ""
     test_3 = "2"
-    ans = solution.letterCombinations(nums=test_3)
+    ans = solution.letterCombinations(digits=test_1)
     print(ans)
  
